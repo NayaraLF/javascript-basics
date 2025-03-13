18a86df75bcdb6f08188223aff85cf1d0305ceb5
Nayara Louback-Franco <66848069+NayaraLF@users.noreply.github.com>
2025-03-13 10:00:28 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,17 @@
+// Aprendendo Repetições com While
+
+/* var c = 1
+while (c <= 10) {
+    console.log(`Passo ${c}`)
+    c++
+
+}
+*/
+
+// Aprendendo Repetições com Do While
+
+var c = 1 
+do {
+    console.log(`Passo ${c}`)
+    c++
+}while (c <= 10)
